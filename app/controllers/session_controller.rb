f35566dea96ee7531d2dcdf895ac1ad91c1a4192
Doug Kintop <dkintop@hotmail.com>
2020-04-15 14:04:36 -0400

    adds functionality to generate a JWT and send it in the response object of a login request at session#create

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,14 +4,19 @@ class SessionController < ApplicationController
 
     def create
         
-        @user = User.find_by(email: params[:user][:email])
+        @user = User.find_by(email: session_params[:email])
         
-        if @user && @user.authenticate(params[:user][:password])
+        if @user && @user.authenticate(session_params[:password])
+
+            token = JWT.encode(@user, ENV["JWT_SECRET"])
+
+            
             render json: {
                 email: @user.email, 
                 name: @user.name, 
                 discipline: @user.discipline,
-                id: @user.id
+                id: @user.id,
+                token: token
             } 
         else
             render json:{
@@ -25,5 +30,9 @@ class SessionController < ApplicationController
 
     end
 
+    private
+     def session_params
+      params.require(:user).permit(:email, :password)
+    end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,11 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
   # GET /users
-  def index
-    @users = User.all
+  # def index
+  #   @users = User.all
 
-    render json: @users
-  end
+  #   render json: @users
+  # end
 
   # GET /users/1
   def show
@@ -25,13 +25,13 @@ class UsersController < ApplicationController
   end
 
   # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @user.update(user_params)
+  #     render json: @user
+  #   else
+  #     render json: @user.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /users/1
   def destroy
